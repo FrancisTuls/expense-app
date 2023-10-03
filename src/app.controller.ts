d3df60170ab127aed23498f36d1985174d7537af
Francis Tulang <91929009+FrancisTuls@users.noreply.github.com>
2023-10-03 19:09:07 +0800

    [FEATURE] Added a Controller with Endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
-@Controller()
+@Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('')
+  getAllIncomeReports() {
+    return [];
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':id')
+  getIncomeReportById() {
+    return {};
+  }
+
+  @Post('')
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Deleted';
   }
 }
