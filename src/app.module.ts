aabcd5568b48343f5cdf5c4563a7549219bbd601
Francis Tulang <91929009+FrancisTuls@users.noreply.github.com>
2023-10-05 17:32:40 +0800

    Revert "[FINAL]Finalized the structure and added a summary report service"
    This reverts commit 99ac75129d77ad8b21d117919e45894e4c588d97.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,9 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { SummaryModule } from './summary/summary.module';
-
-import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [SummaryModule, ReportModule],
+  imports: [],
   controllers: [AppController],
   providers: [
     AppService,
